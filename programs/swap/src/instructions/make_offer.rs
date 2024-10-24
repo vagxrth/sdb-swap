0834e3798a9a84883ddbc790bc01bbb019901b08
Vagarth <vagarth419@gmail.com>
2024-10-24 18:33:59 +0530

    feat: create vault and offer account

--- programs/swap/src/instructions/make_offer.rs
+++ programs/swap/src/instructions/make_offer.rs
@@ -1,7 +1,43 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{token_interface::{ Mint, TokenAccount, TokenInterface }, associated_token::AssociatedToken};
 
 #[derive(Accounts)]
-pub struct MakeOffer {}
+pub struct MakeOffer<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+    #[account[mint::token_program = token_program]]
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account[mint::token_program = token_program]]
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = maker,
+        space = ANCHOR_DISCRIMINATOR + Offer::INIT_SPACE,
+        seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub offer: Account<'info, SystemAccount>,
+
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = offer,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>
+}
 
 pub fn send_offered_tokens_to_vault(ctx: Context<MakeOffer>) -> Result<()> {
     msg!("Greetings from: {{:?}}", ctx.program_id);
